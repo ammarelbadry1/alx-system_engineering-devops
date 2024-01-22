623ce81b7a345e437287dbbbd1dfdf77d2ed6f54
ammarelbadry1 <ammarelbadry1@gmail.com>
2024-01-23 01:36:07 +0200

    tasks upload

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
 # This manifest install flask from pip3
 
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => ['/usr/bin/'],
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
